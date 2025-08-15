3ed2f188900a8ee07e608202b90eee7f4c1281d8
Kevin Teodoro <akio.kevin@gmail.com>
2025-08-15 21:50:29 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
-// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/telefone-operadora-brasil/', // 🔹 fix para GitHub Pages
+  base: '/telefone-operadora-brasil/',
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' && componentTagger(),
+    mode === 'development' &&
+    componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
